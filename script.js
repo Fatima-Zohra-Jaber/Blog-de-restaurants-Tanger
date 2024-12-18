b320edff8bd60e11b544afb5fd01cb8e0f7065c0
Fatima-Zohra-Jaber <jaber.fatiima.zohra@gmail.com>
2024-12-18 16:53:44 +0100

    V2

--- script.js
+++ script.js
@@ -10,22 +10,79 @@ async function fetchRestaurants() {
     }
 }
 
-// Fonction pour afficher les restaurants dans la page
+
+
 function displayRestaurants(data) {
     const restaurantsElement = document.getElementById("restaurants");
-    
+
     data.forEach((restaurant) => {
         const restaurantElement = document.createElement("div");
+        restaurantElement.className='restaurant';
         restaurantElement.innerHTML = `
-            <img src="${restaurant.photo}" alt="${restaurant.nom}">
+            <img src="${restaurant.photo}" alt="${restaurant.nom}" >
             <h2>${restaurant.nom}</h2>
-            <p>Note : ${restaurant.notation}</p>
-            <p>Téléphone : ${restaurant.tel}</p>
+            <p>${restaurant.specialite}</p>
+            <div class="review-container">
+                <span>Notation:</span>
+                ${createStarRating(restaurant.notation)}
+                <span class="note">${restaurant.notation}</span>
+            </div>
+            <a href="restaurant.html?restaurant=${encodeURIComponent(JSON.stringify(restaurant))}" >
+            Détails </a>
         `;
         restaurantsElement.appendChild(restaurantElement);
     });
 }
 
+
+
+
 // Charger les restaurants au chargement de la page
 fetchRestaurants();
 
+// Fonction pour créer les étoiles
+function createStarRating(note) {
+const filledWidth = (note / 5) * 100; // Pourcentage d'étoiles pleines
+return `
+    <div class="stars">
+    <div class="filled" style="width: ${filledWidth}%;">★★★★★</div>
+    </div>
+`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname.endsWith("restaurant.html")) {
+        detailsRestaurant(); 
+    }
+    if (window.location.pathname.endsWith("admin.html")) {
+        admin(); 
+    }
+
+});
+
+// Fonction pour afficher les détails d'un restaurant
+function detailsRestaurant(){
+    let urlParams = new URLSearchParams(new URL(window.location.href).search);
+    const restaurantJson = decodeURIComponent(urlParams.get('restaurant'));
+    let restaurant = JSON.parse(restaurantJson); 
+    const main= document.querySelector("main");
+    const restaurantDetail= document.createElement('section');
+    restaurantDetail.className ='restaurantDetail';
+    
+    restaurantDetail.innerHTML = ` 
+            <div class="imageDetail">
+                <img src="${restaurant.photo}" alt="">
+            </div>
+               
+            <div class="textDetail">
+                <h1>${restaurant.nom}</h1>
+                <p><span>Spécialiste: </span>${restaurant.specialite}</p>
+                <p><span>Notation: </span>${restaurant.notation}</p>
+                <p><span>Avis: </span>${restaurant.avis}</p>
+                <p><span>Adresse: </span>${restaurant.adresse}</p>
+                <p><span>Tel: </span>${restaurant.tel}</p>
+                <p><span>E-mail: </span>${restaurant.email}</p>
+                <p><span>Site web: </span>${restaurant.site}</p>
+            </div>`;
+    main.appendChild(restaurantDetail);
+}
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 
 const express = require('express');
-const cors = require('cors'); // Importer le module CORS
+const cors = require('cors');
 const app = express();
-const fs = require('fs');
+const fs = require('fs');// Importer le module file system
 const PORT = 3000;
 
 // Activer CORS
 app.use(cors());
 
-// Charger les données JSON
-let restaurants = require('./data.json');
-
 // Middleware JSON
 app.use(express.json());
 
-// Endpoint pour récupérer tous les restaurants
-app.get('/restaurants', (req, res) => {
-    res.json(restaurants);
+// Charger les données JSON
+// let restaurants = require('./data.json'); // c'est just poure la lecture
+
+const data = fs.readFileSync('data.json'); // c'est just poure la lecture et l'écriture
+let restaurants= JSON.parse(data);
+
+app.get('/', (req, res) => {
+    const message = "Welcome To our server";
+    res.send(message);
 });
 
 // Lancer le serveur
@@ -25,61 +28,61 @@ app.listen(PORT, () => {
 });
 
 // // Récupérer tous les restaurants
-// app.get('/restaurants', (req, res) => {
-//     res.json(restaurants);
-// });
+app.get('/restaurants', (req, res) => {
+    res.json(restaurants);
+});
 
-// // Récupérer un restaurant par nom
-// app.get('/restaurants/:nom', (req, res) => {
-//     const restaurant = restaurants.find(r => r.nom.toLowerCase() === req.params.nom.toLowerCase());
-//     if (!restaurant) return res.status(404).send('Restaurant non trouvé');
-//     res.json(restaurant);
-// });
+// Récupérer un restaurant par nom
+app.get('/restaurants/:nom', (req, res) => {
+    const restaurant = restaurants.find(r => r.nom.toLowerCase() === req.params.nom.toLowerCase());
+    if (!restaurant) return res.status(404).send('Restaurant non trouvé');
+    res.json(restaurant);
+});
 
 // // Ajouter un restaurant
-// app.post('/restaurants', (req, res) => {
-//     const nouveauRestaurant = {
-//         id: Date.now(), // Ajout d'un identifiant unique
-//         nom: req.body.nom,
-//         specialite: req.body.specialite,
-//         adresse: req.body.adresse,
-//         notation: req.body.notation,
-//         avis: req.body.avis || [],
-//         site: req.body.site,
-//         tel: req.body.tel
-//     };
+app.post('/restaurants', (req, res) => {
+    const newRestaurant = {
+        id: restaurants.length + 1,
+        nom: req.body.nom,
+        specialite: req.body.specialite,
+        adresse: req.body.adresse,
+        notation: req.body.notation,
+        avis: req.body.avis || [],
+        site: req.body.site,
+        tel: req.body.tel
+    };
 
-//     restaurants.push(nouveauRestaurant);
-//     fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(restaurants, null, 2)); // Sauvegarder les changements dans le fichier
-//     res.status(201).json(nouveauRestaurant);
-// });
+    restaurants.push(newRestaurant);
+    fs.writeFileSync('data.json', JSON.stringify(restaurants, null, 2));
+    res.status(201).json(newRestaurant);
+    
+});
 
 // // Modifier un restaurant
-// app.put('/restaurants/:id', (req, res) => {
-//     const restaurant = restaurants.find(r => r.id === parseInt(req.params.id)); // Utilisation de l'id pour la correspondance
-//     if (!restaurant) return res.status(404).send('Restaurant non trouvé');
+app.put('/restaurants/:id', (req, res) => {
+    const restaurant = restaurants.find(r => r.id === parseInt(req.params.id)); 
+    if (!restaurant) return res.status(404).send('Restaurant non trouvé');
 
-//     // Mettre à jour les informations du restaurant
-//     restaurant.nom = req.body.nom || restaurant.nom;
-//     restaurant.specialite = req.body.specialite || restaurant.specialite;
-//     restaurant.adresse = req.body.adresse || restaurant.adresse;
-//     restaurant.notation = req.body.notation || restaurant.notation;
-//     restaurant.avis = req.body.avis || restaurant.avis;
-//     restaurant.site = req.body.site || restaurant.site;
-//     restaurant.tel = req.body.tel || restaurant.tel;
+    // Mettre à jour les informations du restaurant
+    restaurant.nom = req.body.nom || restaurant.nom;
+    restaurant.specialite = req.body.specialite || restaurant.specialite;
+    restaurant.adresse = req.body.adresse || restaurant.adresse;
+    restaurant.notation = req.body.notation || restaurant.notation;
+    restaurant.avis = req.body.avis || restaurant.avis;
+    restaurant.site = req.body.site || restaurant.site;
+    restaurant.tel = req.body.tel || restaurant.tel;
 
-//     fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(restaurants, null, 2)); // Sauvegarder les changements dans le fichier
-//     res.json(restaurant);
-// });
+    fs.writeFileSync( 'data.json', JSON.stringify(restaurants, null, 2));
+    res.json(restaurant);
+});
+
 
 // // Supprimer un restaurant
-// app.delete('/restaurants/:id', (req, res) => {
-//     const index = restaurants.findIndex(r => r.id === parseInt(req.params.id)); // Utilisation de l'id pour la correspondance
-//     if (index === -1) return res.status(404).send('Restaurant non trouvé');
+app.delete('/restaurants/:id', (req, res) => {
+    const index = restaurants.findIndex(r => r.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).send('Restaurant non trouvé');
 
-//     restaurants.splice(index, 1);
-//     fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(restaurants, null, 2)); // Sauvegarder les changements dans le fichier
-//     res.sendStatus(204); // Pas de contenu
-// });
-
-
+    restaurants.splice(index, 1);
+    fs.writeFileSync('data.json', JSON.stringify(restaurants, null, 2));
+    res.sendStatus(204); // res.status(204).send();
+});
