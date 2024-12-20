1f8aff7533a779e572d6efa9dc55a7cbe9550f29
Fatima-Zohra-Jaber <jaber.fatiima.zohra@gmail.com>
2024-12-20 12:58:06 +0100

    V3

---
+++ restaurant.js
@@ -0,0 +1,54 @@
+
+import {createStarRating} from './script.js';
+
+
+// Exemple d'utilisation
+const ratingHTML = createStarRating(4.5);
+
+// Fonction pour récupérer les données de l'API
+async function fetchRestaurant() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nomRestaurant = urlParams.get('nom');
+
+    try {
+        const response = await fetch(`http://localhost:3000/restaurants/${encodeURIComponent(nomRestaurant)}`);
+        if (!response.ok) {
+            throw new Error('Restaurant non trouvé');
+        }
+        const restaurant = await response.json();
+        detailsRestaurant(restaurant);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des détails du restaurant :", error);
+    }
+}
+
+// Fonction pour afficher les détails d'un restaurant
+function detailsRestaurant(restaurant){ 
+    const main= document.querySelector("main");
+    const restaurantDetail= document.createElement('section');
+    restaurantDetail.className ='restaurantDetail';
+    
+    restaurantDetail.innerHTML = ` 
+            <div class="imageDetail">
+                <img src="${restaurant.photo}" alt="">
+            </div>
+               
+            <div class="textDetail">
+                <h1>${restaurant.nom}</h1>
+                <p><span>Spécialiste: </span>${restaurant.specialite}</p>
+                <div class="review-container">
+                                <span>Notation:</span>
+                                ${createStarRating(restaurant.notation)}
+                                <span class="note">${restaurant.notation}</span>
+                            </div>
+                <p><span>Avis: </span>${restaurant.avis}</p>
+                <p><span>Adresse: </span>${restaurant.adresse}</p>
+                <p><span>Tel: </span>${restaurant.tel}</p>
+                <p><span>E-mail: </span>${restaurant.email}</p>
+                <p><span>Site web: </span>${restaurant.site}</p>
+            </div>`;
+    main.appendChild(restaurantDetail);
+}
+
+
+fetchRestaurant(); 
--- script.js
+++ script.js
@@ -2,15 +2,13 @@
 // Fonction pour récupérer les données de l'API
 async function fetchRestaurants() {
     try {
-        const response = await fetch("http://localhost:3000/restaurants"); // Faire une requête GET vers l'API
-        const data = await response.json(); // Convertir la réponse en JSON
-        displayRestaurants(data); // Appeler une fonction pour afficher les données
+        const response = await fetch("http://localhost:3000/restaurants"); 
+        const data = await response.json(); 
+        displayRestaurants(data); 
     } catch (error) {
         console.error("Erreur lors de la récupération des restaurants :", error);
     }
 }
-
-
 
 function displayRestaurants(data) {
     const restaurantsElement = document.getElementById("restaurants");
@@ -27,21 +25,19 @@ function displayRestaurants(data) {
                 ${createStarRating(restaurant.notation)}
                 <span class="note">${restaurant.notation}</span>
             </div>
-            <a href="restaurant.html?restaurant=${encodeURIComponent(JSON.stringify(restaurant))}" >
-            Détails </a>
+            <button onclick="window.location.href='restaurant.html?nom=${encodeURIComponent(restaurant.nom)}'">
+                Détails</button>
+            
         `;
         restaurantsElement.appendChild(restaurantElement);
     });
 }
 
-
-
-
 // Charger les restaurants au chargement de la page
 fetchRestaurants();
 
 // Fonction pour créer les étoiles
-function createStarRating(note) {
+export function createStarRating(note) {
 const filledWidth = (note / 5) * 100; // Pourcentage d'étoiles pleines
 return `
     <div class="stars">
@@ -50,39 +46,5 @@ return `
 `;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.location.pathname.endsWith("restaurant.html")) {
-        detailsRestaurant(); 
-    }
-    if (window.location.pathname.endsWith("admin.html")) {
-        admin(); 
-    }
 
-});
 
-// Fonction pour afficher les détails d'un restaurant
-function detailsRestaurant(){
-    let urlParams = new URLSearchParams(new URL(window.location.href).search);
-    const restaurantJson = decodeURIComponent(urlParams.get('restaurant'));
-    let restaurant = JSON.parse(restaurantJson); 
-    const main= document.querySelector("main");
-    const restaurantDetail= document.createElement('section');
-    restaurantDetail.className ='restaurantDetail';
-    
-    restaurantDetail.innerHTML = ` 
-            <div class="imageDetail">
-                <img src="${restaurant.photo}" alt="">
-            </div>
-               
-            <div class="textDetail">
-                <h1>${restaurant.nom}</h1>
-                <p><span>Spécialiste: </span>${restaurant.specialite}</p>
-                <p><span>Notation: </span>${restaurant.notation}</p>
-                <p><span>Avis: </span>${restaurant.avis}</p>
-                <p><span>Adresse: </span>${restaurant.adresse}</p>
-                <p><span>Tel: </span>${restaurant.tel}</p>
-                <p><span>E-mail: </span>${restaurant.email}</p>
-                <p><span>Site web: </span>${restaurant.site}</p>
-            </div>`;
-    main.appendChild(restaurantDetail);
-}
