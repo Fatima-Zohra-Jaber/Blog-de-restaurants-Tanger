7e4efe8c43e8a7c34bd27472d3039225d24544dd
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2025-08-04 16:38:28 +0100

    Update restaurant.js

--- frontend/js/restaurant.js
+++ frontend/js/restaurant.js
@@ -5,7 +5,7 @@ async function fetchRestaurant() {
     const idRestaurant = urlParams.get('id');
 
     try {
-        const response = await fetch(`http://localhost:3000/restaurants/id/${idRestaurant}`);
+        const response = await fetch(`restaurants/id/${idRestaurant}`);
         if (!response.ok) {
             throw new Error('Restaurant non trouvé');
         }
